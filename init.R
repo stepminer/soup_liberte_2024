f2688d6953d1171c29d3e71d89215c72319dead4
stepminer <70709034+stepminer@users.noreply.github.com>
2024-09-29 01:48:40 -0400

    Update init.R

--- init.R
+++ init.R
@@ -3,11 +3,11 @@
 # R code to install packages if not already installed
 #
 # CRAN version
-install.packages('leaflet')
+#install.packages('leaflet')
 
 # Or Github version
-#if (!require('devtools')) install.packages('devtools')
-#devtools::install_github('rstudio/leaflet')
+if (!require('devtools')) install.packages('devtools')
+devtools::install_github('rstudio/leaflet')
  
 helpers.installPackages('dplyr', 'ggplot2','shiny', 'leaflet', 'readr')
 
