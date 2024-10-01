a73a6c00c4c942d59ae4e21bb94e1417f382f1c0
stepminer <70709034+stepminer@users.noreply.github.com>
2024-10-01 17:49:26 -0400

    Update init.R

--- init.R
+++ init.R
@@ -3,14 +3,14 @@
 # R code to install packages if not already installed
 #
 # CRAN version
- apt-get install -y make          
- apt-get install -y pandoc        
- apt-get install -y libpng-dev     
- apt-get install -y libgdal-dev    
- apt-get install -y gdal-bin      
- apt-get install -y libgeos-dev   
- apt-get install -y libproj-dev   
- apt-get install -y libsqlite3-dev 
+ apt-get install   make          
+ apt-get install   pandoc        
+ apt-get install   libpng-dev     
+ apt-get install   libgdal-dev    
+ apt-get install   gdal-bin      
+ apt-get install   libgeos-dev   
+ apt-get install   libproj-dev   
+ apt-get install   libsqlite3-dev 
 
 
 
